c90f80da8d8ad4848a7fa9727095ea59594e86f3
bernardohazin <bernardo.hazin@gmail.com>
2022-02-16 20:27:54 -0300

    [refactor] exporting missing get-transaction-details controller and prisma implementation

--- api/src/infra/db/prisma/index.ts
+++ api/src/infra/db/prisma/index.ts
@@ -1,1 +1,2 @@
 export * from "./prisma-list-transactions-repository";
+export * from "./prisma-get-transaction-details-repository";
--- api/src/presentation/controllers/index.ts
+++ api/src/presentation/controllers/index.ts
@@ -1,1 +1,2 @@
 export * from "./list-transactions-controller";
+export * from "./get-transaction-details-controller";
