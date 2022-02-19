0e113b952c2405bb0ceb6dca49fea6c9c32ef0c1
bernardohazin <bernardo.hazin@gmail.com>
2022-02-19 18:14:31 -0300

    [feat] adding transaction details repository test

---
+++ api/tests/infra/db/prisma/get-transaction-details-repository.spec.ts
@@ -0,0 +1,45 @@
+import { PrismaGetTransactionDetailsRepository } from "@/infra/db/prisma";
+import { makeTransactionMock } from "@/tests/domain/mocks";
+import { Transaction } from "@/domain/entities";
+import prisma from "@/main/prisma";
+
+const createRepository = () => {
+  return new PrismaGetTransactionDetailsRepository(prisma);
+};
+
+describe("Infra | DB | Prisma | Get transaction details repository", function () {
+  let transactions: Transaction[] = [];
+  const repository = createRepository();
+
+  beforeAll(async () => {
+    await prisma.$connect();
+
+    transactions = [makeTransactionMock(), makeTransactionMock()];
+
+    await prisma.transaction.createMany({
+      data: transactions,
+    });
+  });
+
+  afterAll(async () => {
+    const deleteTransactions = prisma.transaction.deleteMany();
+
+    await prisma.$transaction([deleteTransactions]);
+
+    await prisma.$disconnect();
+  });
+
+  test("Should return transaction details from database based on transaction id", async () => {
+    const [transaction] = transactions;
+    const result = await repository.execute(transaction.id);
+
+    expect(result).toStrictEqual(transaction);
+  });
+
+  test("Should return null if transaction does not exists", async () => {
+    const nonExistentId = "6";
+    const result = await repository.execute(nonExistentId);
+
+    expect(result).toBeNull();
+  });
+});
